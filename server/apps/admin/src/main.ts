64357616928e886034bf63f11535b59da6b54604
qpzp <qpzp@hotmail.com>
2019-10-25 23:19:53 +0800

    使用swagger和装饰器编写接口文档

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -1,9 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log(`http://localhost:3000 listen`)
+    const app = await NestFactory.create(AppModule);
+
+    const options = new DocumentBuilder()
+        .setTitle('全栈之巅-后台管理API')
+        .setDescription('供后台管理界面调用的服务端API')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-docs', app, document);
+
+    await app.listen(3000);
+    console.log(`http://localhost:3000/api-docs`);
 }
+
 bootstrap();
--- server/libs/db/src/models/user.model.ts
+++ server/libs/db/src/models/user.model.ts
@@ -1,9 +1,17 @@
-import {prop} from '@typegoose/typegoose';
+import {modelOptions, prop} from '@typegoose/typegoose';
+import {ApiModelProperty} from "@nestjs/swagger";
 
+@modelOptions({
+    schemaOptions: {
+        timestamps: true,
+    }
+})
 export class User {
+    @ApiModelProperty({description: '用户名', example: 'user1'})
     @prop()
     username: string;
 
+    @ApiModelProperty({description: '密码', example: 'pass1'})
     @prop()
     password: string;
 
