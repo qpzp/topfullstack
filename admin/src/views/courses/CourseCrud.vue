1d9d16ff0fa23f29769764077965f0f8b6c2c619
qpzp <qpzp@hotmail.com>
2019-11-09 13:29:22 +0800

    配合Avue事件完成CRUD接口对接

--- admin/src/views/courses/CourseCrud.vue
+++ admin/src/views/courses/CourseCrud.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <avue-crud :data="data.data" :option="option"></avue-crud>
+    <avue-crud
+      :data="data.data"
+      :option="option"
+      @row-save="create"
+      @row-update="update"
+      @row-del="remove"
+    ></avue-crud>
   </div>
 </template>
 
@@ -16,14 +22,30 @@
     option = {
       title: '课程管理',
       column: [
-        {prop: 'name',label:'课程名称'},
-        {prop: 'cover',label:'课程封面图'},
+        {prop: 'name', label: '课程名称'},
+        {prop: 'cover', label: '课程封面图'},
       ]
     };
 
     async fetch() {
       const res = await this.$http.get('courses',);
       this.data = res.data;
+    }
+
+    async create(row, done, loading) {
+      await this.$http.post('courses', row);
+      this.$message.success('创建成功');
+      this.fetch();
+      done();
+    }
+
+    async update(row, index, done, loading) {
+      const data = JSON.parse(JSON.stringify(row));
+      delete data.$index;
+      await this.$http.put(`courses/${row._id}`, data);
+      this.$message.success('更新成功');
+      this.fetch();
+      done();
     }
 
     async remove(row) {
